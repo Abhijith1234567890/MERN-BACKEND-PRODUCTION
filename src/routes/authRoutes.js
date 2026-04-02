5b3081836ce3bb64f4951c14058e813650eaeae1
abhijith <abhijithpp2001@gmail.com>
2026-04-02 12:15:06 +0530

    validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const { register, login, getTasks } = require("../controllers/authController");
 
 const authMiddleware = require("../middleware/authMiddleware");
+const validate = require("../middleware/validateMiddelware");
+const { registerValidation, loginValidation } = require("../validators/authValidator");
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register",registerValidation, validate, register);
+router.post("/login",loginValidation, validate, login);
 
 // Protected route
 router.get("/tasks", authMiddleware, getTasks);
