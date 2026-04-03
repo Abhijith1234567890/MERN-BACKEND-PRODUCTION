1a48b6749fd57f9b1577c66368731c28cf98a8b9
abhijith <abhijithpp2001@gmail.com>
2026-04-03 21:57:44 +0530

    LOGGING

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+
 const AppError = require("./utils/AppError");
 const errorMiddleware = require("./middleware/errorMiddleware");
 
 const app = express();
 
+// Create logs folder if not exists
+const logDir = path.join(__dirname, "logs");
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+// Morgan stream (write to file)
+const accessLogStream = fs.createWriteStream(path.join(logDir, "combined.log"), {
+  flags: "a",
+});
+
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// Log all requests
+app.use(morgan("combined", {stream: accessLogStream}))
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
