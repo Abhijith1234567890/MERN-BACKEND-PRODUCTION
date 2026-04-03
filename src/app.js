590b1d4d81cb30fbc7b1882b7b052532a810fd8e
abhijith <abhijithpp2001@gmail.com>
2026-04-03 19:01:45 +0530

    PAGINATION_FILTERING_SEARCH

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
 // Handle unknown routes
 app.use((req, res, next) => {
