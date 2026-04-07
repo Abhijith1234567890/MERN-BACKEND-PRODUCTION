3436cd04f874a29cd3c5975e7c79c5f9b4f00745
abhijith <abhijithpp2001@gmail.com>
2026-04-07 11:19:05 +0530

    Merge branch 'main' of https://github.com/Abhijith1234567890/MERN-BACKEND-PRODUCTION

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
+const rateLimit = require("express-rate-limit");
+
+// Limit login/register requests
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // max 5 requests
+  message: {
+    status: "fail",
+    message: "Too many requests, try again later",
+  },
+});
+
 const { register, login, getTasks } = require("../controllers/authController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 const validate = require("../middleware/validateMiddelware");
-const { registerValidation, loginValidation } = require("../validators/authValidator");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validators/authValidator");
 
-router.post("/register",registerValidation, validate, register);
-router.post("/login",loginValidation, validate, login);
+// Apply limiter
+router.post("/register", authLimiter, registerValidation, validate, register);
+router.post("/login",authLimiter, loginValidation, validate, login);
 
 // Protected route
 router.get("/tasks", authMiddleware, getTasks);
